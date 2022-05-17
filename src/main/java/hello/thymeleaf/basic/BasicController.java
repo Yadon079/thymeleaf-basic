8705eba603c7ff754c139f3b2375171ca773d873
yadon079 <zeidgame@gmail.com>
2022-05-17 19:10:05 +0900

    feature : 블록, 자바스크립트 인라인
    <description>
    * 타임리프 - 기본 기능
    - 블록
    >> th:block
    - 자바스크립트 인라인
    >> script th:inline="javascript"
    
    <issue-number>

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -147,4 +147,19 @@ public class BasicController {
 
     return "basic/comments";
   }
+
+  @GetMapping("/block")
+  public String block(Model model) {
+    addUsers(model);
+
+    return "basic/clock";
+  }
+
+  @GetMapping("/javascript")
+  public String javascript(Model model) {
+    model.addAttribute("user", new User("userA", 10));
+    addUsers(model);
+
+    return "basic/javascript";
+  }
 }
